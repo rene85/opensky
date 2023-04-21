21ebb9ef0727e9076f17028cd5f74c046d29843a
René Crapts <rene@crapts.eu>
2023-04-21 18:23:50 +0200

    Set up code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
+        'prettier', // put prettier last
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
