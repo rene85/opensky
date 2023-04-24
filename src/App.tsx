6b8b3d22ac0364e1bc2e8405340a04f948c32ac0
René Crapts <rene@crapts.eu>
2023-04-24 13:39:08 +0200

    Clean up view

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import './App.css'
+
+import { AxiosResponse } from 'axios'
 import { useEffect, useState } from 'react'
 import {
     Observable,
@@ -12,15 +15,9 @@ import {
     switchMap,
 } from 'rxjs'
 import { sampleOpenSkyResponse } from './assets/sample'
-import { axiosGet } from './wrap/axios'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import { AxiosResponse } from 'axios'
-import './App.css'
 import { init, withGetAllStatesResponse } from './model/model'
 import { OpenSkyResponse } from './opensky/response'
+import { axiosGet } from './wrap/axios'
 
 const interval$ = (updateIntervalMs: number) => {
     const initialImmediateTick$ = from(['initial'])
@@ -94,34 +91,8 @@ function App({ useSample: useSampleData }: App) {
         }
     }, [])
 
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
             {model.callsigns.map(([id, callsign]) => (
                 <p key={id.string}>{callsign}</p>
             ))}
